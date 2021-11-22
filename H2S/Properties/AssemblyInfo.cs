b445b7b28c45444db2c7dd0db89041f81d0e7570
Kevin Gut <kevin.gut@bitm.sg>
2021-11-22 21:57:15 +0100

    Update Version

--- H2S/Properties/AssemblyInfo.cs
+++ H2S/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("H2S")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Http2Socks is a reverse proxy for onion service accessibility")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AyrA.ch")]
 [assembly: AssemblyProduct("H2S")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © AyrA.ch 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
